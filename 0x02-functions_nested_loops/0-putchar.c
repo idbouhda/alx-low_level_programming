133252c651895836c4a8136f1b82f93be5b09e87
idbouhda <loubna.idbouhda10@gmail.com>
2023-06-20 05:48:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "_putchar.c"
 
 /**
   * main - Entry point
@@ -7,10 +7,13 @@
   */
 int main(void)
 {
-	_putchar('m');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
 	_putchar('a');
-	_putchar('i');
-	_putchar('n');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
