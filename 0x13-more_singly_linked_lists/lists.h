ecc4e1eb23a1f99a55ce97ac754e17127e724a96
idbouhda <loubna.idbouhda10@gmail.com>
2023-08-01 14:18:29 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
-#define  LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 /**
  * struct listint_s - singly linked list
@@ -8,12 +12,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *for Holberton project
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -31,4 +35,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif
+
+#endif /*LISTS_H*/
