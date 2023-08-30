1568437471a1c47ef82b56e8d8ebe8f65aaeaa0a
idbouhda <loubna.idbouhda10@gmail.com>
2023-08-30 12:53:22 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,15 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-int i = 0;
-
-if (i  < 48)
-{
 _putchar('\n');
-_putchar(s[i]);
-i++;
+
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
 return;
 }
-	_puts_recursion(s+1);
 }
 
