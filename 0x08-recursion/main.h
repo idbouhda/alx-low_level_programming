73e6b3a181b3019e873563f4aba4b1d30d0266fc
idbouhda <loubna.idbouhda10@gmail.com>
2023-08-30 09:51:32 -0700

    putchar

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,17 +10,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 #endif /* MAIN_H */
