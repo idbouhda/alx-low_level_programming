6effe717c5d319a3ce2f5a887a99c5982dee789f
idbouhda <loubna.idbouhda10@gmail.com>
2023-08-31 08:23:44 -0700

    edite sqrt func

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,21 +5,17 @@
  * @i: integer
  * @j: integer
  *
- * Return:  Always 0.
+ * Return:  On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int natural_sqrt(int i, int j)
 {
 if (i * i > j)
-{
 return (-1);
-}
 else if (i * i == j)
-{
 return (i);
+return (natural_sqrt(i + 1, j));
 }
-natural_sqrt(i + 1, j);
-}
-
 /**
  * _sqrt_recursion -  returns the natural square root of a number
  * @n: integer
