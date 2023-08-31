b5fb8f90051c4b9cce6e10f25c903c915ed82fb4
idbouhda <loubna.idbouhda10@gmail.com>
2023-08-31 07:59:44 -0700

    added func sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,24 @@
+#include <stdio.h>
 #include "main.h"
+/**
+ * natural_sqrt - returns the root of a natural number.
+ * @i: integer
+ * @j: integer
+ *
+ * Return:  Always 0.
+ */
+int natural_sqrt(int i, int j)
+{
+if (i * i > j)
+{
+return (-1);
+}
+else if (i * i == j)
+{
+return (i);
+}
+natural_sqrt(i + 1, j);
+}
 
 /**
  * _sqrt_recursion -  returns the natural square root of a number
@@ -9,15 +29,5 @@
  */
 int _sqrt_recursion(int n)
 {
-int m = 1;
-if (m * m >  n)
-{
-return (-1);
-}
-else if (m * m == n)
-{
-return (1);
-}
-m = m + 1;
-return (_sqrt_recursion(n));
+return (_sqrt_recursion(natural_sqrt(1, n)));
 }
