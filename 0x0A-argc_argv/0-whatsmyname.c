3008774161e6bed69866a9842a3ab5e0560b98fe
idbouhda <loubna.idbouhda10@gmail.com>
2023-09-01 11:41:49 -0700

    void func

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *  printname -  prints the name of program.
+ *
+ * Return: Always 0.
+ */
+
+void printname(void)
+{
+char str[] = "whatsmyname";
+puts(str);
+printf("\n");
+}
