c9b36fd61a9fd544b1778f25c3daa37382b9923d
idbouhda <loubna.idbouhda10@gmail.com>
2023-09-04 15:17:07 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  program that adds positive numbers.
+ * @ac: integer
+ * @av: character
+ *
+ * Return: Always 0 or 1.
+ */
+int main(int ac, char  **av)
+{
+int k = 0;
+char *c;
+
+if (av[1] == NULL || av[2] == NULL)
+{
+return (printf("0\n"));
+}
+while (--ac)
+
+{
+for (c = ac[av]; *c; c++)
+if ( *c < '0' || *c > '9')
+{
+printf("Error\n");
+return (1);
+}
+k += atoi(av[ac]);
+}
+printf("%d\n", k);
+return (0);
+}
