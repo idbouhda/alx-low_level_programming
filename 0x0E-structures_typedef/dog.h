6c0ed9c3761e7a866ddecc503dac939a28661d59
idbouhda <loubna.idbouhda10@gmail.com>
2023-09-12 08:41:44 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - owner of dog
+ * @name: dog name
+ * @age: his age
+ * @owner: his owner
+ *
+ * Description: informaition about dog
+ */
 
 struct dog
 {
-char * name;
+char *name;
 float age;
-char * owner;
+char *owner;
 }
 #endif /* DOG_H */
