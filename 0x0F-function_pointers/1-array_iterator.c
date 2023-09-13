4f8da62f6de3ad9e985f6f2f00822c1f0160908f
idbouhda <loubna.idbouhda10@gmail.com>
2023-09-13 14:19:04 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,14 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+unsigned int j;
 unsigned int i;
 for (i = 0; i < size; i++)
 {
 (*action)(array[i]);
+}
+for (j = 0; j < size; j++)
+{
+printf("0x%x\n", array[j]);
 }
 }
