482c5b77abd7cdf74f3c31c08e7821c2a2bb7d0e
idbouhda <loubna.idbouhda10@gmail.com>
2023-09-13 08:50:08 -0700

    function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
